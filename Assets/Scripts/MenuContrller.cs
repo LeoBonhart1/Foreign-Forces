66c361fa10b2328867b235738a6f866873736d1f
Ege Lagap <gumaokle@gmail.com>
2023-06-09 07:04:37 +0300

    level design + ingame UI

--- Assets/Scripts/MenuContrller.cs
+++ Assets/Scripts/MenuContrller.cs
@@ -18,8 +18,13 @@ public class MenuContrller : MonoBehaviour
   [SerializeField] private float defaultBrightness = 1;
 
   [Header("Resolution Dropdown")]
-  public TMP_Dropdown resolutionDropdown;
+  [SerializeField] private TMP_Dropdown resolutionDropdown;
+
   private Resolution[] resolutions;
+  private List<Resolution> filteredResolutions;
+
+  private int currentResolutionIndex = 0;
+  private float currentRefreshRate;
 
   private int qualityLevel;
   private bool isFullScreen;
@@ -28,28 +33,38 @@ public class MenuContrller : MonoBehaviour
   private void Start()
   {
     resolutions = Screen.resolutions;
+    filteredResolutions = new List<Resolution>();
+
     resolutionDropdown.ClearOptions();
+    currentRefreshRate = Screen.currentResolution.refreshRate;
 
-    List<string> options = new List<string>();
-
-    int currentResolutionIndex = 0;
-
-    for (int i = 0; i< resolutions.Length; i++)
+    for (int i = 0; i < resolutions.Length; i++)
     {
-       string option = resolutions[i].width + " x " + resolutions[i].height;
-       options.Add(option);
-
-       if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
-       {
-          currentResolutionIndex =i;
-       }
+        if (resolutions[i].refreshRate == currentRefreshRate)
+        {
+            filteredResolutions.Add(resolutions[i]);
+        }
     }
 
+    List<string> options = new List<string>();
+    for (int i = 0; i < filteredResolutions.Count; i++)
+    {
+      string resolutionOption = filteredResolutions[i].width + "x" + filteredResolutions[i].height + " " + filteredResolutions[i].refreshRate + " Hz";
+      options.Add(resolutionOption);
+      if (filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height)
+      {
+          currentResolutionIndex = i;
+      }
+    }
+
+    resolutionDropdown.AddOptions(options);
+    resolutionDropdown.value = currentResolutionIndex;
+    
   }
 
   public void SetResolution(int resolutionIndex)
   {
-     Resolution resolution = resolutions[resolutionIndex];
+     Resolution resolution = filteredResolutions[resolutionIndex];
      Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
   }
 
